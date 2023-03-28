a3045d6a20332a37c7568efb667b7bc40cd0e490
Chatchai Paisalpanich <frinttropystudy222@gmail.com>
2023-03-28 14:57:24 +0700

    Added FarAxisOrigin2DPositionCalculator

--- src/room.py
+++ src/room.py
@@ -1,17 +1,53 @@
 from abc import ABC, abstractmethod
-import numbers
-
+import math
 
 class Position:
-    def __init__(self,x: float, y: float, z: float):
+    def __init__(self, x: float, y: float, z: float):
         self._x = x
         self._y = y
         self._z = z
 
     
-    def is_null(self):
+    def is_null(self) -> bool:
         return False
     
+    
+    def is_equal(self, other_pos) -> bool:
+        other_x = other_pos.get_x()
+        other_y = other_pos.get_y()
+        other_z = other_pos.get_z()
+        
+        return other_x == self._x and other_y == self._y and other_z == self._z
+        
+
+    def get_dist_to(self, other_pos) -> float:
+        o_x = other_pos.get_x()
+        o_y = other_pos.get_y()
+        o_z = other_pos.get_z()
+        
+        return math.sqrt( (self._x - o_x) ** 2 + (self._y - o_y) ** 2 + (self._z - o_z) ** 2)
+
+    
+    def is_x_orthogonal(self, other_pos) -> float:
+        o_y = other_pos.get_y()
+        o_z = other_pos.get_z()
+        
+        return self._y == o_y and self._z == o_z
+    
+    
+    def is_y_orthogonal(self, other_pos) -> float:
+        o_x = other_pos.get_x()
+        o_z = other_pos.get_z()
+
+        return self._x == o_x and self._z == o_z
+    
+
+    def is_z_orthogonal(self, other_pos) -> float:
+        o_x = other_pos.get_x()
+        o_y = other_pos.get_y()
+        
+        return self._x == o_x and self._y == o_y
+
 
     def to_string(self) -> str:
         return f"x:{self._x}, y: {self._y}, z: {self._z}"
@@ -41,6 +77,14 @@ class Position:
         return self._z
     
 
+    def __str__(self):
+        return self.to_string()
+    
+
+    def __repr__(self):
+        return self.to_string()
+    
+
 class NullPosition(Position):
 
 
@@ -57,34 +101,34 @@ class NullPosition(Position):
     
 
     def set_x(self, x: float):
-        raise 
+        raise NotImplementedError
     
 
     def set_y(self, y: float):
-        self._y = y
+        raise NotImplementedError
 
 
     def set_z(self, z: float):
-        self._z = z
+        raise NotImplementedError
 
 
     def get_x(self) -> float:
-        return self._x
+        raise NotImplementedError
     
 
     def get_y(self) -> float:
-        return self._y
+        raise NotImplementedError
     
 
     def get_z(self) -> float:
-        return self._z
+        raise NotImplementedError
 
 
 class PositionCalculator(ABC):
 
 
     @abstractmethod
-    def get_position_or_none(self, many_dist_to_anchor) -> Position:
+    def get_position_or_null_position(self, many_tag_dist_to_anchor) -> Position:
         raise NotImplementedError
     
 
@@ -92,7 +136,7 @@ class FarAxisOrigin2DPositionCalculator(PositionCalculator):
     """
     dist_to_anchor = [
         [3.24, Position(0, 0, 0)],
-        [6.42, Position(12.5, 0, 0)],
+        [15.42, Position(12.5, 0, 0)],
         [9.12, Position(0, 7.7, 0)],
     ]
     """
@@ -101,37 +145,86 @@ class FarAxisOrigin2DPositionCalculator(PositionCalculator):
         pass
 
     
-    def get_position_or_null_position(self, many_dist_to_anchor) -> Position:
-        if self._is_position_calculatable(many_dist_to_anchor):
-            return self._calculate_position(many_dist_to_anchor)
-        else:
+    def get_position_or_null_position(self, many_tag_dist_to_anchor_pos) -> Position:
+        try:
+            return self._get_position(many_tag_dist_to_anchor_pos)
+        except Exception as e:
+            print(e)
             return NullPosition()
         
 
-    def _is_position_calculatable(self, many_dist_to_anchor) -> bool:
-        if not self._is_many_dist_to_anchor_correct_format(many_dist_to_anchor):
-            return False
-        if not self._is_many_dist_to_anchor_has_orthogonal_anchor(many_dist_to_anchor):
-            return False
-        if not self._is_many_dist_to_anchor_has_origin_anchor(many_dist_to_anchor):
-            return False
-    
-    def _is_many_dist_to_anchor_correct_format(self, many_dist_to_anchor) -> bool:
-        pass
-
-
-    def _calculate_position(self, many_dist_to_anchor) -> Position:
-        pass
+    # def _is_position_calculatable(self, many_tag_dist_to_anchor_pos) -> bool:
+    #     if not self._is_many_tag_dist_to_anchor_pos_correct_format(many_tag_dist_to_anchor_pos):
+    #         return False
+    #     if not self._is_many_tag_dist_to_anchor_pos_has_origin_anchor_pos(many_tag_dist_to_anchor_pos):
+    #         return False
+    #     if not self._is_many_tag_dist_to_anchor_pos_has_far_x_anchor_pos(many_tag_dist_to_anchor_pos):
+    #         return False
+    #     if not self._is_many_tag_dist_to_anchor_pos_has_far_y_anchor_pos(many_tag_dist_to_anchor_pos):
+    #         return False
         
     
+    # def _is_many_dist_to_anchor_correct_format(self, many_dist_to_anchor_pos) -> bool:
+    #     pass
 
-class TagSensor:
-    def __init__(self, id):
-        self.id = id
-        self.s
 
-class AnchorSensor:
-    pass
+    def _get_position(self, many_tag_dist_and_anchor_pos) -> Position:
+        # tag_dist_to_origin_pos = self._get_tag_dist_to_origin_pos(many_tag_dist_to_anchor_pos)
+        # tag_dist_to_far_x_pos = self._get_tag_dist_to_far_x_pos(many_tag_dist_to_anchor_pos)
+        # tag_dist_to_far_y_pos = self._get_tag_dist_to_far_y_pos(many_tag_dist_to_anchor_pos)
+
+        # origin_pos = self._get_origin_pos(many_tag_dist_to_anchor_pos)
+        # far_x_pos = self._get_far_x_pos(many_tag_dist_to_anchor_pos)
+        # far_y_pos = self._get_far_y_pos(many_tag_dist_to_anchor_pos)
+
+        tag_dist_to_origin_pos, origin_pos = self._get_tag_dist_to_origin_and_origin_pos(many_tag_dist_and_anchor_pos)
+        tag_dist_to_far_x_pos, far_x_pos = self._get_tag_dist_to_far_x_and_far_x_pos(many_tag_dist_and_anchor_pos)
+        tag_dist_to_far_y_pos, far_y_pos = self._get_tag_dist_to_far_y_and_far_y_pos(many_tag_dist_and_anchor_pos)
+
+        origin_dist_to_far_x = origin_pos.get_dist_to(far_x_pos)
+        origin_dist_to_far_y = origin_pos.get_dist_to(far_y_pos)
+        
+        tag_x_coord = self._get_single_axis_coordinate(origin_dist_to_far_x, tag_dist_to_origin_pos, tag_dist_to_far_x_pos)
+        tag_y_coord = self._get_single_axis_coordinate(origin_dist_to_far_y, tag_dist_to_origin_pos, tag_dist_to_far_y_pos)
+
+        tag_pos = Position(tag_x_coord, tag_y_coord, 0)
+        return tag_pos
+
+
+    def _get_tag_dist_to_origin_and_origin_pos(self, many_tag_dist_and_anchor_pos):
+        for tag_dist, anchor_pos in many_tag_dist_and_anchor_pos:
+            if anchor_pos.is_equal(Position(0, 0, 0)):
+                return tag_dist, anchor_pos
+            
+        raise Exception("Tag distance and anchor pos don't have origin anchor")
+    
+
+    def _get_tag_dist_to_far_x_and_far_x_pos(self, many_tag_dist_and_anchor_pos):
+        origin = Position(0, 0, 0)
+        for tag_dist, anchor_pos in many_tag_dist_and_anchor_pos:
+            if origin.is_x_orthogonal(anchor_pos) and anchor_pos.get_x() != 0:
+                return tag_dist, anchor_pos
+            
+        raise Exception("Tag distance and anchor pos don't have far x anchor")
+    
+
+    def _get_tag_dist_to_far_y_and_far_y_pos(self, many_tag_dist_and_anchor_pos):
+        origin = Position(0, 0, 0)
+        for tag_dist, anchor_pos in many_tag_dist_and_anchor_pos:
+            if origin.is_y_orthogonal(anchor_pos) and anchor_pos.get_y() != 0:
+                return tag_dist, anchor_pos
+            
+        raise Exception("Tag distance and anchor pos don't have far y anchor")
+
+
+    def _get_single_axis_coordinate(self, origin_dist_to_far_axis, tag_dist_to_origin, tag_dist_to_far_axis) -> float:
+        of = origin_dist_to_far_axis
+        to = tag_dist_to_origin
+        tf = tag_dist_to_far_axis
+
+        coord = ( (tf * tf) - (to * to) - (of * of) ) / (-2 * of)
+        return coord
+
 
 class Room:
 
