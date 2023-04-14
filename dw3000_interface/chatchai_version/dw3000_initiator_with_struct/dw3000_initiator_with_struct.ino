e2481daa0dbeaf9b6d31fcb4cc3a822e94f50b0d
Chatchai Paisalpanich <frinttropystudy222@gmail.com>
2023-04-14 15:24:41 +0700

    Added Initiator

--- dw3000_interface/chatchai_version/dw3000_initiator_with_struct/dw3000_initiator_with_struct.ino
+++ dw3000_interface/chatchai_version/dw3000_initiator_with_struct/dw3000_initiator_with_struct.ino
@@ -14,19 +14,51 @@ const uint8_t PIN_INTERRUPT_REQUEST = 34;
 const uint8_t PIN_SLAVE_SELECT = 4;
 
 
+
+
 typedef struct Responder {
-    uint8_t id;
-    uint8_t frame_sequence_number;
+    uint8_t _id;
+    uint8_t _frame_sequence_number;
 };
 
 void Responder_initialize(Responder *self, uint8_t in_id){
-    self->id = in_id;
-    self->frame_sequence_number = 0;
+    self->_id = in_id;
+    self->_frame_sequence_number = 0;
 }
 
 uint8_t Responder_get_id(Responder *self){
-    return self->id;
+    return self->_id;
 }
+
+uint8_t Responder_get_frame_sequence_number(Responder *self){
+    return self->_frame_sequence_number;
+}
+
+uint8_t Responder_set_frame_sequence_number(Responder *self, uint8_t in_frame_seq_num){
+    self->_frame_sequence_number = in_frame_seq_num;
+}
+
+void Responder_print(Responder *self){
+    Serial.print("responder: \n");
+    Serial.print("    id: "); Serial.println(self->_id);
+    Serial.print("    frame_sequence_number: "); Serial.println(self->_frame_sequence_number);
+}
+
+
+
+
+typedef struct Initiator {
+    uint8_t _id;
+};
+
+void Initiator_initialize(Initiator *self, uint8_t in_id){
+    self->_id = in_id;
+}
+
+uint8_t Initiator_get_id(Initiator *self){
+    return self->_id;
+}
+
 
 
 
@@ -46,7 +78,8 @@ void loop() {
 
     Responder my_responder;
     Responder_initialize(&my_responder, 100);
-    Serial.print(Responder_get_id(&my_responder));
+    Responder_set_frame_sequence_number(&my_responder, 66);
+    Responder_print(&my_responder);
     
     Serial.print("End of main loop \n");
 }
