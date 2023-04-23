d4ca28a45268f3fc314a443bed5cb2849402b92a
Chatchai Paisalpanich <frinttropystudy222@gmail.com>
2023-04-23 13:53:09 +0700

    Added main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,18 @@
 import room
+import redis_message_broker
 
 def main():
-    pass
+    redis_msg_broker = redis_message_broker.RedisMessageBroker("0.0.0.0")
+    r = room.Room()
+    tag_pos_pub = room.TagPositionPublisher(redis_msg_broker)
+    room_range_updater = room.RoomRangeUpdater(r)
+
+    r.add_observer(tag_pos_pub)
+    room_range_updater.set_message_broker(redis_msg_broker)
+
+    while(True):
+        pass
+
     
 
 
