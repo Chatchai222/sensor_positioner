dc956e0acb4e2bb5c3b0578f6b89b937e74a6fbb
Chatchai Paisalpanich <frinttropystudy222@gmail.com>
2023-04-28 15:07:53 +0700

    Added dw3000 initiator and responder specifically for analysis

--- dw3000_interface/analysis/dw3000_initiator_analysis/dw3000_initiator_analysis.ino
+++ dw3000_interface/analysis/dw3000_initiator_analysis/dw3000_initiator_analysis.ino
@@ -511,8 +511,8 @@ void DW3000Chip_initialize_for_initiator(){
 
 
 // Ranger
-const int _Ranger_MAX_RANGING_ATTEMPT_BEFORE_GIVING_UP = 1000;
-const uint32_t _Ranger_DELAY_BETWEEN_RANGING_ATTEMPT_IN_MILLISECOND = 5;
+const int _Ranger_MAX_RANGING_ATTEMPT_BEFORE_GIVING_UP = 5000;
+const uint32_t _Ranger_DELAY_BETWEEN_RANGING_ATTEMPT_IN_MILLISECOND = 0;
 int _Ranger_ranging_attempt_count = 0;
 
 int Ranger_get_recent_ranging_attempt_count(){
@@ -724,14 +724,14 @@ void my_debug() {
 void setup() {
     // put your setup code here, to run once:
     DW3000Chip_initialize_for_initiator();
-    connect_to_wifi(NETWORK_NAME, NETWORK_PASSWORD);
+    //connect_to_wifi(NETWORK_NAME, NETWORK_PASSWORD);
     
     
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
-    Serial.println("Start of loop initiator with struct");
+    //Serial.println("Start of loop initiator with struct");
 
     struct Initiator initiator;
     initiator_initialize(&initiator, INITIATOR_ID);
@@ -745,14 +745,15 @@ void loop() {
         Serial.println("ranger failed to get distance");
     } else {
         Serial.print(distance); Serial.print(","); Serial.println(Ranger_get_recent_ranging_attempt_count());
-        Serial.print("distance: "); Serial.println(distance);
-        Serial.print("ranging_attempt_count: "); Serial.println(Ranger_get_recent_ranging_attempt_count());
-        publish_to_UDP(&initiator, responder_ptr, distance);
+        //Serial.print("distance: "); Serial.println(distance);
+        //Serial.print("ranging_attempt_count: "); Serial.println(Ranger_get_recent_ranging_attempt_count());
+        //publish_to_UDP(&initiator, responder_ptr, distance);
     }
-    initiator_print(&initiator);
-    responder_print(responder_ptr);
+    //initiator_print(&initiator);
+    //responder_print(responder_ptr);
     
-    Serial.println("End of loop initiator with struct");
+    //Serial.println(String("wtf there is a string?"));
+    //Serial.println("End of loop initiator with struct");
     Sleep(1);
 
 }
